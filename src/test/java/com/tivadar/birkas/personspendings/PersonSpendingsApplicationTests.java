20b5bb3ca31e901ee32798bb9dcc9a51f25e7a19
BirkasTivadar <birkastivadar@gmail.com>
2021-07-29 10:29:13 +0200

    Initial commit

---
+++ src/test/java/com/tivadar/birkas/personspendings/PersonSpendingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tivadar.birkas.personspendings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonSpendingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
